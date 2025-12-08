bf697c48fa66b62a86627e801cddf6272d42c731
Björn Weghenkel <bjoern.weghenkel@bcxp.de>
2025-12-08 17:43:46 +0100

    Add dummy access token endpoint

--- src/app/conversation.api.ts
+++ src/app/conversation.api.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversationApi {
-
   constructor() { }
+  public getAccessToken(): Observable<number> {
+    return of(123)
+  }
 }
--- src/app/conversation.service.ts
+++ src/app/conversation.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ConversationApi } from './conversation.api';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversationService {
-
-  constructor(private readonly conversationApi: ConversationApi) { }
+  private readonly accessToken$: BehaviorSubject<number | null> = new BehaviorSubject<number | null>(null)
+  constructor(private readonly conversationApi: ConversationApi) {
+    conversationApi.getAccessToken().subscribe(accessToken => {
+      this.accessToken$.next(accessToken)
+    })
+  }
 }
