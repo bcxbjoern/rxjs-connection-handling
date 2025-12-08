053dd36923eacaff69d45bee7a6765109fa65806
Björn Weghenkel <bjoern.weghenkel@bcxp.de>
2025-12-08 18:21:09 +0100

    Add interface

--- src/app/conversation.api.ts
+++ src/app/conversation.api.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
-import {Observable, of} from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+export interface AccessToken {
+  token: string;
+  expires_in: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConversationApi {
-  constructor() { }
-  public getAccessToken(): Observable<number> {
-    return of(123)
+  constructor() {}
+
+  public getAccessToken(): Observable<AccessToken> {
+    const token: AccessToken = {
+      token: '123',
+      expires_in: 60,
+    };
+    return of(token);
   }
 }
--- src/app/conversation.service.ts
+++ src/app/conversation.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ConversationApi } from './conversation.api';
+import { AccessToken, ConversationApi } from './conversation.api';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ConversationService {
-  private readonly accessToken$: BehaviorSubject<number | null> = new BehaviorSubject<
-    number | null
-  >(null);
-  constructor(private readonly conversationApi: ConversationApi) {
+  private readonly accessToken$: BehaviorSubject<AccessToken | null> =
+    new BehaviorSubject<AccessToken | null>(null);
+
+  constructor(conversationApi: ConversationApi) {
     conversationApi.getAccessToken().subscribe((accessToken) => {
       this.accessToken$.next(accessToken);
     });
