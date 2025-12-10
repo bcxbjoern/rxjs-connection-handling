975a4897ba08a4f2969ea8c39f5022b1bec8ec3f
bjoernwe <bjoernwe@users.noreply.github.com>
2025-12-10 21:37:50 +0100

    Use signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
 import { ConversationService } from './conversation.service';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
-  imports: [AsyncPipe, JsonPipe],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('react-playground');
   conversationService = inject(ConversationService);
-  protected token$ = this.conversationService.token$;
-  protected messages$ = this.conversationService.conversation$;
+  protected token = toSignal(this.conversationService.token$);
+  protected messages = toSignal(this.conversationService.conversation$);
 }
