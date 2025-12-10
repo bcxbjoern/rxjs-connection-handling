f280919baa3941a09efd0405e752eccc407aea6b
bjoernwe <bjoernwe@users.noreply.github.com>
2025-12-10 21:43:41 +0100

    Add basic styling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  effect,
+  inject,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { ConversationService } from './conversation.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 
@@ -8,9 +16,32 @@ import { toSignal } from '@angular/core/rxjs-interop';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('react-playground');
   conversationService = inject(ConversationService);
   protected token = toSignal(this.conversationService.token$);
   protected messages = toSignal(this.conversationService.conversation$);
+
+  @ViewChild('messageContainer') messageContainer!: ElementRef;
+
+  constructor() {
+    effect(() => {
+      if (this.messages() && this.messageContainer) {
+        this.scrollToBottom();
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.scrollToBottom();
+  }
+
+  private scrollToBottom(): void {
+    try {
+      this.messageContainer.nativeElement.scrollTop =
+        this.messageContainer.nativeElement.scrollHeight;
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
